0e8c26302120f28278f623c09dac24a4f418e1e9
Thomas Ayissi <thomas8sea@gmail.com>
2023-05-26 00:50:13 +0200

    Updating Icons -Menu -Searching on website

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Content} from "../../interfaces/content/content";
 
 @Component({
   selector: 'app-card',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
+
+  constructor() {
+
+  }
+
+
 
 }
