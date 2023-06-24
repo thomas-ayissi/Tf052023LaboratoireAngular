48f00b6a5149d4193de661d0a9f8099d571b84f9
Thomas Ayissi <thomas8sea@gmail.com>
2023-06-24 10:23:43 +0200

    "card" component deleted After that we still have have some errors about card component references. So we delete card component references and all problems are solved. Our app currently works !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component';
 import { TextComponent } from './components/text/text.component';
 import { UserComponent } from './components/user/user.component';
 import { LoginComponent } from './components/login/login.component';
