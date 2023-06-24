d4471e4dffa9766bad79eed856f6669f103728f1
Thomas Ayissi <thomas8sea@gmail.com>
2023-06-24 12:12:22 +0200

    Update done All 5 files related tpo "ap root" are placed in the same root folder. So, now, we have : 1/ App Component html 2/ App Component css 3/ App Component ts 4/ App Module Ts 5/ App Rooting Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './components/app.component.html',
-  styleUrls: ['./components/app.component.scss']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Tf052023LaboratoireAngular';
