f5d784f84e0c37b09e07310a2861f66c594a6070
Thomas Ayissi <thomas8sea@gmail.com>
2023-06-26 22:31:14 +0200

    Like and unlike button switch functionnal
    real all article and Reread all article fuctionnal alse

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -12,8 +12,10 @@ export class CardComponent implements OnInit {
   square_reads!: number;
   square_imageURL!: string;
   square_summary!: string;
+  square_text_button!: string;
+  square_text_like_button!: string;
   square_full_text!: string;
-  square_text-button!:string;
+
 
   ngOnInit(): void {
     this.square_title = 'Wim Robberecht (SGRS): «Des hackings visent des infrastructures critiques européennes»';
@@ -22,6 +24,8 @@ export class CardComponent implements OnInit {
     this.square_reads = 10;
     this.square_imageURL = '/../assets/pictures/sgrs.jpg'
     this.square_summary = 'Le vice-amiral Wim Robberecht a pris la tête du SGRS en août 2021 suite à la crise « Jürgen Conings ». Son service publie aujourd’hui le tout premier rapport annuel de son histoire.';
+    this.square_text_button = 'Lire la suite';
+    this.square_text_like_button = 'j\'aime';
     this.square_full_text = 'Le vice-amiral Wim Robberecht, patron du SGRS, est le premier à publier un rapport annuel déclassifié pour son service. Signe d’un besoin d’ouverture et d’affirmation de son service de renseignement dans un monde polarisé. ' +
       'Il vise le doublement de ses effectifs d’ici cinq ans.\n' +
       '\n' +
@@ -32,7 +36,7 @@ export class CardComponent implements OnInit {
       'Tout ce qu’on décrit sous le vocabulaire erroné de « désinformation » ou de « fake news », pour nous c’est de l’ingérence étrangère par la manipulation de l’information. ' +
       'On utilise pour cela les réseaux sociaux, qui n’existaient tout simplement pas lors de la guerre froide. Les moyens cyber rendent possible de prendre le contrôle des outils d’une firme : sabotages, espionnage et hackings. ' +
       'Il y a des hackings tous les jours. De temps en temps, on pense avoir à faire à des ransomware, mais cela peut dissimuler une tentative d’accès à des infrastructures critiques.'
-    this.square_text-button = 'j\'aime';
+
   }
 
   /*export class CardComponent {
@@ -43,13 +47,24 @@ export class CardComponent implements OnInit {
     this.router.navigate(['/article', articleId]);
   };*/
 
-  onClickReads() {
-    this.square_reads++;
+  onLike () {
+    if (this.square_text_like_button === 'J\'aime') {
+      this.square_text_like_button = 'Je n\'aime plus'
+    } else {
+      this.square_text_like_button = 'J\'aime';
+    }
+
   }
 
-  onClick() {
-    this.onClick(/*article.id£*/);
+  onClickReads ()
+  {
+    if (this.square_text_button === 'Lire la suite') {
+      this.square_reads++;
+      this.square_text_button = 'Relire la suite ?';
+    } else {
+      this.square_reads++;
+      this.square_text_button = 'Relire la suite ?';
+    }
   }
 
 }
-
