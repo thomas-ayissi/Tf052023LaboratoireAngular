5155b6c02002c4c4a34e009a05f4753a2f980be2
Thomas Ayissi <thomas8sea@gmail.com>
2023-07-04 12:06:52 +0200

    Correctly Add -> environment file -> environment folder -> environment (file and folder) references is included inside tsconfig.app.json and inside main.ts CORRECT VERSION - Everything is working properly

---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false, // Ou true en fonction de votre environnement
+  // Autres variables d'environnement...
+};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+
+  enableProdMode();
+
+}
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+
+/*
+
+--DOWN WORKING CODE CONTAINED HERE BEFORE REPLACE IT BY ADDING NEW ENI BOOK CODE--
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -7,3 +26,10 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+--DOWN WORKING NEW ENI BOOK CODE--
+
+
+
+ */
