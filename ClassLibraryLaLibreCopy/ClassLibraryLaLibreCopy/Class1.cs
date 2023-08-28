697c83a5c71df0df90a5420a1847d44b12e04f51
Thomas Ayissi <thomas8sea@gmail.com>
2023-08-28 19:07:50 +0200

    Cancellation TF URL in app.component.html file. Now we have empty spaces

--- ClassLibraryLaLibreCopy/ClassLibraryLaLibreCopy/Class1.cs
+++ ClassLibraryLaLibreCopy/ClassLibraryLaLibreCopy/Class1.cs
@@ -1,5 +1,29 @@
 ﻿namespace ClassLibraryLaLibreCopy;
 
-public class Class1
+using System.Data;
+using System.Data.SqlClient;
+
+string connectionString = "Data Source=server;Initial Catalog=database;User ID=username;Password=password;";
+using (SqlConnection connection = new SqlConnection(connectionString))
 {
+    connection.Open();
+    string query = "SELECT * FROM TableName";
+    using (SqlCommand command = new SqlCommand(query, connection))
+    {
+        using (SqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                // Traitement des données retournées
+            }
+        }
+    }
 }
+
+
+/*public class Class1
+{
+
+} */
+
+
